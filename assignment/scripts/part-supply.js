f94ef0215728f58cde899225eb4ea5234b368e36
Neil Hanson <pro.neilhanson@gmail.com>
2022-04-15 10:39:34 -0500

    answered all questions.

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -48,10 +48,18 @@ for (i = 0; i < supplyChanges.length + 1; i++){
 console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop. 
 console.log('7. Showing supplyChanges with "for of" loop');
+for (parts of supplyChanges){
+    console.log(parts);
+}
 
 // 8. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
 console.log('8. Total supplies available is:');
+let totalSupplies = 0;
+for (parts of supplyChanges){
+    totalSupplies = totalSupplies + parts;
+}
+console.log(totalSupplies);
 
 // 9. We have a large stash of parts in our warehouse that we 
 //    need to box up and get ready for shipment. 
@@ -60,3 +68,10 @@ console.log('8. Total supplies available is:');
 //    no more boxes can be filled.
 //    Then log how many boxes were filled, and how many parts are left over.
 console.log('9. Filling boxes with a "while" loop');
+let stash = 572;
+let boxes = 0;
+while (stash > 7){
+    stash -= 7;
+    boxes += 1;
+}
+console.log(`${boxes} boxes were filled and ${stash} parts are left over.`);
